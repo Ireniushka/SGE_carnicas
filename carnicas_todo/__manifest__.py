5522783706805e9c4bf062a7e1c2e0642e8e16c4
AlbertoRG97 <alberto.rgmb@gmail.com>
2021-02-19 05:28:28 +0100

    campo de autocalculo

--- carnicas_todo/__manifest__.py
+++ carnicas_todo/__manifest__.py
@@ -1,6 +1,8 @@
 {
     "name":"Control de animales",
     "description":"Este modulo tiene como funcion llevar un registro del sacrificio de los animales para las cárnicas, pudiendo generar tambien informes",
+    "author":"Irene Rengel, Alberto Ruiz, Luis Rosell",
+    "category":"Control de mataderos",
     "data":[
         'views/views.xml',
         'security.xml',
--- carnicas_todo/animalmodel.py
+++ carnicas_todo/animalmodel.py
@@ -1,11 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class animal(models.Model):
     __name = "animal"
 
-    raza = fields.Char("Raza")
-    edad = fields.Char("Edad")
-    matadero = fields.Selection(selection=[("cadiz", "Cádiz"),("jerez","Jerez"),("sevilla","Sevilla")])
-    fecha_sacr = fields.Date("Fecha_sacr")
-    precio = fields.Float("Precio")
+    raza = fields.Char("Raza",required=True)
+    edad = fields.Integer("Edad",required=True)
+    peso = fields.Integer("Peso", required=True)
+    matadero = fields.Selection(selection=[("cadiz", "Cádiz"),("jerez","Jerez"),("sevilla","Sevilla")],required=True)
+    fecha_sacr = fields.Date("Fecha_sacr",required=True)
+    precio = fields.Float("Precio", compute="_precioTotal", store=True)
     ult_revision = fields.Date("Ultima revision")
+
+    @api.depends("peso", "edad")
+    def _precioTotal(self):
+        for anm in self:
+            anm.precio = anm.peso + (10*anm.edad)
